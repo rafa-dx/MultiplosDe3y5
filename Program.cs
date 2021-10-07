c2e452c33e0fe71d89f801eb6acec003ed12742b
Jesús Dorantes <jesus.dorantes.89@gmail.com>
2021-10-07 01:51:52 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplosDe3y5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            int final = 1000;
+            Console.WriteLine("Multiplos de 3 y 5");
+            for (int x=3;x<=final;x++)
+            { 
+                if(x%3==0 || x%5==0)
+                {
+                    Console.WriteLine(" "+ x);
+                    suma += x;
+                }
+            }
+            Console.WriteLine("la suma de los multiplos es: " + suma);
+            Console.ReadLine();
+        }
+    }
+}
